fc5ede3586912f6ad2490118a45393c6a2a61f8e
prisha <prishakhichadi@gmail.com>
2026-03-15 16:43:25 +0530

    timer added

--- monkeytype.sh
+++ monkeytype.sh
@@ -14,7 +14,6 @@ generate_target() {
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[33m'
-blue='\033[0;34m'
 nc='\033[0m' # no colour
 nc_bold='\033[1m'
 green_bold='\033[1;32m'
@@ -22,45 +21,79 @@ green_bold='\033[1;32m'
 easy="success is not final, failure is not fatal: it is the courage to continue that counts."
 
 while true; do
+
+    clear
+    echo -e "${nc_bold}choose mode${nc}"
+    echo "1) word based"
+    echo "2) timer based"
+    echo -n "select: "
+    read mode_choice
+
     clear
     echo "select your difficulty"
-    echo "1) easy (10 words)"
-    echo "2) medium (25 words)"
-    echo "3) hard (40 words)"
+    echo "1) easy"
+    echo "2) medium"
+    echo "3) hard"
     echo "choose (1, 2, or 3): "
     read choice
 
+
+
     if [[ "$choice" == "1" ]]; then
         target=$(generate_target 10 2 6)
+        time_limit=20
     elif [[ "$choice" == "2" ]]; then
         target=$(generate_target 25 4 8)
+        time_limit=40
     elif [[ "$choice" == "3" ]]; then
         target=$(generate_target 40 6 12)
+        time_limit=60
     else
         target=$easy
     fi
 
-    typing_test="TYPING TEST"
+    typing_test="typing test!"
+
+    upper_lim=60 #for word mode
 
     clear
-    echo ""
     echo -e "${nc_bold}${typing_test}${nc}"
+    echo -e "${nc_bold}mode: $([[ "$mode_choice" == "1" ]] && echo "word based" || echo "timer based")${nc}"
+    echo -e "time limit: ${yellow}${time_limit} seconds${nc}"
+    echo "remember to click enter before reaching time limit!"
     echo ""
     echo -e "${red}${target}${nc}"
     echo ""
-    echo "press ENTER, then immediately start typing!"
+    echo "press enter, then immediately start typing!"
     read 
     
     start_time=$(date +%s)
-    echo -n "> "
-    read user_input
+
+    if [[ "$mode_choice" == "2" ]]; then
+        echo -e "${red}you have $time_limit seconds!${nc}"
+        echo -n "> "
+        read -t "$time_limit" user_input
+    else
+        echo -e "${green}word mode(Max: ${upper_lim}s)!${nc}"
+        echo -n "> "
+        read -t "$upper_lim" user_input
+    fi
+
+
     end_time=$(date +%s)
 
     # computing
     timetaken=$(( end_time - start_time ))
-    if [ $timetaken -lt 1 ]; then timetaken=1; fi
+
+    if [[ -z "$user_input" ]]; then 
+        echo -e "\n${red}TIME UP!${nc}"
+        user_input=" " # avoid errors
+    fi
 
     chars=${#user_input}
+
+    if [ $timetaken -lt 1 ]; then timetaken=1; fi
+
     wpm=$(( (chars * 60) / (timetaken * 5) ))
     # 5 chars per word is the standard for wpm calculations
 
